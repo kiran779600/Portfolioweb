271d15dcdae529c9369ca7127cf6bdab6604c0a7
kiran779600 <165990117+kiran779600@users.noreply.github.com>
2024-06-20 18:01:44 +0530

    portfolio

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
-import './Header.css'; 
+import './Header.css';
 import { Link as ScrollLink, animateScroll as scroll } from 'react-scroll';
 
 const Header = () => {
@@ -15,18 +15,24 @@ const Header = () => {
         <Typography variant="h6" style={{ flexGrow: 0, fontWeight: 'bold' }}>
           My Portfolio
         </Typography>
-        <Button component={RouterLink} to="/" color="inherit" onClick={scrollToTop}>
+        <Button component={RouterLink} to="/" color="inherit" onClick={scrollToTop} style={{marginLeft:'750px'}}>
           Home
         </Button>
-        <Button component={ScrollLink} to="AboutMe" spy={true} smooth={true} duration={500} color="inherit">
-          About
-        </Button>
-        <Button component={ScrollLink} to="Projects" spy={true} smooth={true} duration={500} color="inherit">
-          Projects
-        </Button>
-        <Button component={ScrollLink} to="Contact" spy={true} smooth={true} duration={500} color="inherit">
-          Contact
-        </Button>
+        <ScrollLink to="aboutMe" spy={true} smooth={true} duration={500}>
+          <Button color="inherit">
+            About
+          </Button>
+        </ScrollLink>
+        <ScrollLink to="projects" spy={true} smooth={true} duration={500}>
+          <Button color="inherit">
+            Projects
+          </Button>
+        </ScrollLink>
+        <ScrollLink to="contact" spy={true} smooth={true} duration={500}>
+          <Button color="inherit">
+            Contact
+          </Button>
+        </ScrollLink>
       </Toolbar>
     </AppBar>
   );
